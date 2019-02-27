265cd50472b7d1c496c426176d94077702559ebb
Juan Bautista Penalva Perez <usu@usu-PC>
2019-02-27 21:42:58 +0100

    Crea métodos en la clase Jugador para realizar cada uno de los movimientos.

--- videoJuego/src/net/instituto/desa/Game.java
+++ videoJuego/src/net/instituto/desa/Game.java
@@ -1,24 +1,10 @@
 package net.instituto.desa;
 
 public class Game {
-	private static final String ABAJO = "Abajo";
-	private static final String ARRIBA = "Arriba";
-	private static final String IZQUIERDA = "Izquierda";
-	private static final String DERECHA = "Derecha";
-	Jugador j;
+	static final String ABAJO = "Abajo";
+	static final String ARRIBA = "Arriba";
+	static final String IZQUIERDA = "Izquierda";
+	static final String DERECHA = "Derecha";
+	Object j;
 
-	public void movimiento(String m) {
-		if (m.equalsIgnoreCase(DERECHA)) {
-			j.setX(j.getX() + 1);
-		}
-		if (m.equalsIgnoreCase(IZQUIERDA)) {
-			j.setX(j.getX() - 1);
-		}
-		if (m.equalsIgnoreCase(ARRIBA)) {
-			j.setY(j.getY() - 1);
-		}
-		if (m.equalsIgnoreCase(ABAJO)) {
-			j.setY(j.getY() + 1);
-		}
-	}
 }
